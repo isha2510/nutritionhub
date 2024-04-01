476a5725a501ef4471b1c2db74097b7f925c176b
isha2510 <ishatripathi80@gmail.com>
2024-04-01 16:58:13 +0100

    feat:added server deployment configuration for netlify

---
+++ server/netlify/functions/api.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import recipeRouter from '../../src/routes/recipe/recipe.router';
+import connectDB from '../../src/config/database';
+import { jwtCheck, userCheck } from '../../src/middleware/auth';
+import chalk from 'chalk';
+import serverless from "serverless-http";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// connect to MongoDB
+connectDB();
+
+// Middleware configuration
+app.use(cors()); // Allow cross-origin requests from any origin (adjust as needed)
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+// auth0 verify token and add auth object to req.auth
+app.use(jwtCheck());
+app.use(userCheck);
+
+// Your API routes
+
+app.use('/api/recipe', recipeRouter);
+// Start the server
+
+export const handler = serverless(app);
